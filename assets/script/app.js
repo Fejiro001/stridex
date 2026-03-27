56ff3822cdbd84b1cc62fe5e90ba65f7d394b094
Fejiro001 <aberefejiro@gmail.com>
2026-03-27 15:55:05 -0500

    Added navbar toggle functionality

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const menuButton = document.querySelector(".menu-toggle");
+const navActions = document.getElementById("nav-actions");
+
+menuButton.addEventListener("click", () => {
+  navActions.classList.toggle("show-nav");
+  if (navActions.classList.contains("show-nav")) {
+    menuButton.children[0].classList.remove("fa-bars-staggered");
+    menuButton.children[0].classList.add("fa-x");
+  } else {
+    menuButton.children[0].classList.add("fa-bars-staggered");
+    menuButton.children[0].classList.remove("fa-x");
+  }
+});
